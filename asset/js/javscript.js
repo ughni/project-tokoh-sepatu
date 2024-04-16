37e602492a8d287dca97fe3247ea6d30d1be279b
mughni.s.kom <mughnimuhammad015@gmail.com>
2024-04-16 10:18:59 +0700

    fitur Baru 16-4-2024

--- asset/js/javscript.js
+++ asset/js/javscript.js
@@ -1,15 +1,86 @@
+
+// saibar start
 // toogel class active
 const navbarNav = document.querySelector('.navbar-nav');
 
-document.querySelector('#humburger-menu').onclick = () => {
+document.querySelector('#humburger-menu').onclick = (e) => {
     navbarNav.classList.toggle('active')
+    e.preventDefault();
 }
 
-// click di luar sidebar untuk mehilangkan navbar
-const hamburger = document.querySelector('#humburger-menu');
+// klik di luar elemen
+const hm = document.querySelector('#humburger-menu');
+const sb = document.querySelector('#search-btn')
+const sc = document.querySelector('#shopping-cart-button')
+
 
 document.addEventListener('click', function (e) {
-    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.classList.remove('active')
     }
+    if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove('active')
+    }
+    if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+        shoppingCart.classList.remove('active')
+    }
 })
+// click di luar sidebar untuk mehilangkan navbar
+
+// saibar end
+
+
+// search start
+
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-btn').onclick = (e) => {
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+}
+
+
+// search end
+
+// tombol shopping
+
+const shoppingCart = document.querySelector('.shopping-cart');
+
+document.querySelector('#shopping-cart-button').onclick = (e) => {
+    shoppingCart.classList.toggle('active')
+    e.preventDefault();
+}
+
+// tombol shopping end
+
+
+// modal box
+
+const modalDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButtons = document.querySelectorAll('.item-detail-button');
+
+itemDetailButtons.forEach((btn) => {
+    btn.onclick = (e) => {
+        modalDetailModal.style.display = 'flex';
+        e.preventDefault();
+    };
+});
+
+
+
+// icons close 
+document.querySelector('.modal .close-icons').onclick = () => {
+    itemDeteilButton.style.display = 'none'
+    e.preventDefault();
+}
+
+//di luar modal
+
+
+window.onclick = (e) => {
+    if (e.target === modalDetailModal) {
+        modalDetailModal.style.display = 'none';
+    }
+}
